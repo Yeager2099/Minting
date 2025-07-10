edb429a5ddc6dee025dcb11088cd49a6f1cc8b00
Yeager2099 <104197243+Yeager2099@users.noreply.github.com>
2025-07-10 15:49:04 +0800

    Update verify.py

--- verify.py
+++ verify.py
@@ -2,24 +2,45 @@ from web3 import Web3
 from eth_account.messages import encode_defunct
 import random
 
-def sign_challenge(challenge):
+def sign_challenge( challenge ):
+
     w3 = Web3()
+
+    """ To actually claim the NFT you need to write code in your own file, or use another claiming method
+    Once you have claimed an NFT you can come back to this file, update the "sk" and submit to codio to 
+    prove that you have claimed your NFT.
     
-    # >>>>> 唯一需要修改的行 <<<<< 
+    This is the only line you need to modify in this file before you submit """
     sk = "0x950dd91788d82b9ca2eb2417d2b26e9a3bea12d0f37b7b5e417ae87a1630f52c"
-    
+
     acct = w3.eth.account.from_key(sk)
-    signed_message = w3.eth.account.sign_message(challenge, private_key=acct.key)
+
+    signed_message = w3.eth.account.sign_message( challenge, private_key = acct.key )
+
     return acct.address, signed_message.signature
 
+
 def verify_sig():
+    """
+        This is essentially the code that the autograder will use to test signChallenge
+        We've added it here for testing 
+    """
+    
     challenge_bytes = random.randbytes(32)
+
     challenge = encode_defunct(challenge_bytes)
-    address, sig = sign_challenge(challenge)
-    return w3.eth.account.recover_message(challenge, signature=sig) == address
+    address, sig = sign_challenge( challenge )
+
+    w3 = Web3()
+
+    return w3.eth.account.recover_message( challenge , signature=sig ) == address
+
 
 if __name__ == '__main__':
+    """
+        Test your function
+    """
     if verify_sig():
-        print("You passed the challenge!")
+        print( f"You passed the challenge!" )
     else:
-        print("You failed the challenge!")
+        print( f"You failed the challenge!" )
